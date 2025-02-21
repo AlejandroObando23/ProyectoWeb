2a4ef56d7bcf326ec7b9ba37f356e30e211b7d18
unknown <matemedranda15@gmail.com>
2025-02-21 08:21:22 -0500

    1.25.0

---
+++ Code/javascript/Servicios/ingresos.js
@@ -0,0 +1,6 @@
+let botonAgregar =  document.getElementById("agregar");
+let modalAgregar = document.getElementById("modalAgregar");
+
+function abrirAgregarIngreso(){
+    modalAgregar.showModal();
+}
