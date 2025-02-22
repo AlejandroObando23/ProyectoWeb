748e74628897af4760c8f11b0a9625a4f4d6b245
Mateo Medranda <matemedranda15@gmail.com>
2025-02-21 20:36:32 -0500

    1.28.0

--- Code/javascript/PaginaPrincipal/cargarPaginas.js
+++ Code/javascript/PaginaPrincipal/cargarPaginas.js
@@ -5,30 +5,30 @@ function cargarPagina(pagina, n) {
     document.getElementById("reportes").classList.remove('active');
 
     const contenido = document.getElementById("contenido");
-    
+
+    // Iniciar la animación de ocultar
     contenido.style.opacity = 0;
 
-    fetch(pagina)
-        .then(response => response.text())
-        .then(data => {
-            setTimeout(() => {
+    // Esperar la duración de la animación antes de cargar el nuevo contenido
+    setTimeout(() => {
+        fetch(pagina)
+            .then(response => response.text())
+            .then(data => {
                 contenido.innerHTML = data;
+                // Volver a mostrar el contenido con opacidad 1
                 contenido.style.opacity = 1;
                 switch(n){
-                    case 0: document.getElementById("inicio").classList.add('active');break;
+                    case 0: document.getElementById("inicio").classList.add('active'); break;
                     case 1: 
                         document.getElementById("ingresos").classList.add('active');
                         inicializarScriptIngresos();
                         break;
-                    case 2: document.getElementById("gastos").classList.add('active');break;
-                    case 3: document.getElementById("reportes").classList.add('active');break;
+                    case 2: document.getElementById("gastos").classList.add('active'); break;
+                    case 3: document.getElementById("reportes").classList.add('active'); break;
                 }
-                
-            }, 300);
-        })
-        .catch(error => console.error("Error al cargar la página:", error));
-
-        
+            })
+            .catch(error => console.error("Error al cargar la página:", error));
+    }, 300); // Este tiempo debe coincidir con la duración de la transición CSS
 }
 
 window.onload = () => cargarPagina('../html/Inicio/inicio.html', 0);
