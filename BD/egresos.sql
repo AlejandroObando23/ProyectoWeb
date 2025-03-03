b12398ac74d31dab10289bef53b15e775910a4f7
AlejandroObando23 <alejoobando21@gmail.com>
2025-03-03 08:39:22 -0500

    1.43.0

--- BD/egresos.sql
+++ BD/egresos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-02-2025 a las 21:32:10
+-- Tiempo de generación: 03-03-2025 a las 13:33:58
 -- Versión del servidor: 8.0.17
 -- Versión de PHP: 7.3.10
 
@@ -34,18 +34,34 @@ CREATE TABLE `egresos` (
   `IdTipo` int(11) NOT NULL,
   `Monto` float NOT NULL,
   `Metodo` varchar(64) NOT NULL,
-  `Estado` varchar(64) NOT NULL,
+  `Estado` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Completado',
   `idUsuario` int(11) NOT NULL,
-  `Descripcion` varchar(128) NOT NULL
+  `Descripcion` varchar(128) NOT NULL,
+  `FechaRegistro` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `egresos`
 --
 
-INSERT INTO `egresos` (`Id`, `Fecha`, `IdTipo`, `Monto`, `Metodo`, `Estado`, `idUsuario`, `Descripcion`) VALUES
-(1, '2025-02-06', 0, 23, 'Efectivo', '', 8, 'sdasd'),
-(2, '2024-12-25', 0, 20, 'Efectivo', '', 8, 'Gasto Vivienda');
+INSERT INTO `egresos` (`Id`, `Fecha`, `IdTipo`, `Monto`, `Metodo`, `Estado`, `idUsuario`, `Descripcion`, `FechaRegistro`) VALUES
+(1, '2025-02-06', 0, 23, 'Efectivo', 'Completado', 8, 'sdasd', '0000-00-00 00:00:00'),
+(2, '2024-12-25', 0, 20, 'Efectivo', 'Completado', 8, 'Gasto Vivienda', '0000-00-00 00:00:00'),
+(3, '2025-03-06', 0, 1.02, 'Efectivo', 'Completado', 8, 'Gasto Casa', '0000-00-00 00:00:00'),
+(4, '2025-03-06', 0, 1.02, 'Efectivo', 'Completado', 8, 'Gasto Casa', '0000-00-00 00:00:00'),
+(5, '2025-03-11', 0, 23, 'Transferencia', 'Completado', 8, 'Gasto Casa', '2025-03-01 21:46:11'),
+(6, '2025-03-12', 0, 1.03, 'Transferencia', 'Completado', 8, 'Gasto Trabajo', '2025-03-03 08:27:09');
+
+--
+-- Disparadores `egresos`
+--
+DELIMITER $$
+CREATE TRIGGER `insertar_fecha_registro` BEFORE INSERT ON `egresos` FOR EACH ROW BEGIN
+    -- Asigna la hora actual a la columna FechaIngreso
+    SET NEW.FechaRegistro = NOW();
+END
+$$
+DELIMITER ;
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +81,7 @@ ALTER TABLE `egresos`
 -- AUTO_INCREMENT de la tabla `egresos`
 --
 ALTER TABLE `egresos`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Code/javascript/Servicios/gastos.js
+++ Code/javascript/Servicios/gastos.js
@@ -1,5 +1,10 @@
 
 let form;
+
+
+let listaEgresos = [];
+let listaTiposEgresos = [];
+let tablaEgresos;
 //Cargar todos los datos de los ingresos en una lista
 
 function abrirAgregarGasto() {
@@ -41,8 +46,174 @@ function guardarDatosEgreso(event){
 //Cargar todos los datos de los tipos de ingresos en una lista
 function gastoscript(){
     let modalAgregargasto = document.getElementById("modalAgregargasto");
+    
+    tablaEgresos = document.getElementById("tablaEgresos").getElementsByTagName('tbody')[0];
+   
     console.log(document.title);
     document.title = "Gastos | MIECONOMIA";
+    fetch("Gasto/gastosLista.php")
+    .then(response => response.json())
+    .then(data => {
+        console.log("Datos recibidos:", data);
+        if (data.error) {
+            
+            console.error("Error en la respuesta del servidor:", data.error);
+           
+        } else {
+            listaEgresos = data;
+            cargarEgresos();
+            cargarTiposEgresos();
+            
+            
+            
+           
+
+        }
+    })
+    
+    .catch(error => console.error("Error en la solicitud fetch:", error));
+    
     
 }
 
+async function cargarTiposEgresos(){
+    return fetch(`Categoria/tipoIngresoLista.php`)
+    .then(response => response.json())
+    .then(data => {
+        console.log("Datos recibidos:", data);
+        if (data.error) {
+            
+            console.error("Error en la respuesta del servidor:", data.error);
+            
+        } else {
+            listaEgresos = data;
+           
+            cargarTiposFormularioEgreso();
+            cerrarAgregarGasto();
+        }
+    })
+    .catch(error => console.error("Error en la solicitud fetch:", error));
+}
+
+function cargarTiposFormularioEgreso() {
+    
+    let select = document.getElementById("selectTipo");
+    let opciones = '<option value="">Seleccione un tipo</option>';
+
+    listaTiposIngreso.forEach(tipo => {
+        opciones += `<option value="${tipo.Id}">${tipo.Nombre}</option>`;
+    });
+
+    console.log("Tipos de ingreso:", opciones);
+
+    select.innerHTML = opciones;
+}
+
+function cargarEgresos() {
+    tablaEgresos.innerHTML = ""; 
+    
+    listaEgresos.forEach((ingreso) => {
+
+        let nuevaFila = tablaEgresos.insertRow();
+        nuevaFila.classList.add("align-middle");
+        let estado = "";
+        let editar = `<div class="d-grid">
+                     <i style="color:red;font-size: 25px;" class="bi bi-x-circle"></i>
+                    <i style="color:green;font-size: 25px;" class="bi bi-check-circle"></i>
+                    </div>`;
+
+        nuevaFila.insertCell(0).innerText = ingreso.Id;
+        nuevaFila.insertCell(1).innerText = ingreso.Fecha;
+        nuevaFila.insertCell(2).innerText = ingreso.Descripcion;
+        nuevaFila.insertCell(3).innerText = "$ " + ingreso.Monto;
+        nuevaFila.insertCell(4).innerText = ingreso.tipo;
+        nuevaFila.insertCell(5).innerText = ingreso.Metodo;
+
+        if(ingreso.Estado == "Completado"){
+            estado = '<p class="bg-success-subtle text-center m-0 p-0">Completado</p>';
+        }else if(ingreso.Estado == "Anulado"){
+            estado = '<p class="bg-danger-subtle text-center m-0 p-0">Anulado</p>';
+        }
+
+        nuevaFila.insertCell(6).innerHTML = estado;
+
+        nuevaFila.insertCell(7).innerHTML = ""+ ingreso.Nombre + " " + ingreso.Apellido;
+        nuevaFila.insertCell(8).innerHTML = ingreso.FechaRegistro;
+
+        let url = ""+ingreso.CodigoQR;
+
+        console.log(url);
+
+        nuevaFila.insertCell(9).innerHTML = `<div class="shadow mx-auto bg-black d-flex justify-content-center align-items-center qr botonQr" onclick="mostrarQR('`+url+`')"></div>`;
+        nuevaFila.insertCell(10).innerHTML = editar;
+    });
+    
+    renderizarTable();
+    
+   
+}
+
+
+function mostrarQR(imagen) {
+    document.getElementById("codigoQr").style.display = "flex";
+    document.getElementById("cargarQr").src=imagen;
+}
+
+function cerrarQR() {
+    document.getElementById("codigoQr").style.display = "none";
+}
+
+
+let itemsPorPagina = 5;
+let actualPagina = 1;
+
+function renderizarTable() {
+    const tableBody = document.getElementById("table-body");
+   
+    const rows = tableBody.getElementsByTagName("tr");
+    
+    let start = (actualPagina - 1) * itemsPorPagina;
+    let end = start + itemsPorPagina;
+    
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].style.display = (i >= start && i < end) ? "" : "none";
+    }
+    
+    renderizarPagination();
+   
+}
+
+function renderizarPagination() {
+    const pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+    let totalPages = Math.ceil(listaIngresos.length / itemsPorPagina);
+    
+    pagination.innerHTML += `<li class='page-item ${actualPagina === 1 ? "disabled" : ""}'>
+                                <a class='page-link' href='#' onclick='changePage(${actualPagina - 1})'>Anterior</a>
+                            </li>`;
+    
+    for (let i = 1; i <= totalPages; i++) {
+        pagination.innerHTML += `<li class='page-item ${actualPagina === i ? "active" : ""}'>
+                                    <a class='page-link' href='#' onclick='changePage(${i})'>${i}</a>
+                                </li>`;
+    }
+    
+    pagination.innerHTML += `<li class='page-item ${actualPagina === totalPages ? "disabled" : ""}'>
+                                <a class='page-link' href='#' onclick='changePage(${actualPagina + 1})'>Siguiente</a>
+
+                                </li>`;
+                                
+                            }
+
+function changePage(page) {
+    const totalPages = Math.ceil(listaIngresos.length / itemsPorPagina);
+    if (page < 1 || page > totalPages) return;
+    actualPagina = page;
+    renderizarTable();
+}
+
+function updateItemsPerPage() {
+    itemsPorPagina = parseInt(document.getElementById("itemsPerPage").value);
+    actualPagina = 1;
+    renderizarTable();
+}
