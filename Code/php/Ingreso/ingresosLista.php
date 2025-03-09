7b55611f965372419b504101c2c3e5ecda7b8f2f
Mateo Medranda <matemedranda15@gmail.com>
2025-03-09 12:43:51 -0500

    2.4.5

--- Code/php/Ingreso/ingresosLista.php
+++ Code/php/Ingreso/ingresosLista.php
@@ -1,6 +1,22 @@
 <?php
     include("../conexion.php");
-    $consulta = 'SELECT i.Id, i.Fecha, c.Nombre AS "tipo", i.Monto, i.Metodo, i.Estado, u.Nombre, u.Apellido, c.CodigoQR, i.FechaRegistro, i.Descripcion FROM ingresos i INNER JOIN usuarios u ON i.idUsuario = u.Id INNER JOIN categorias c ON i.IdTipo = c.Id  WHERE c.tipo = "ingreso";';
+    $consulta = 'SELECT 
+    i.Id, 
+    i.Fecha, 
+    c.Nombre AS "tipo", 
+    i.Monto, 
+    i.Metodo, 
+    i.Estado, 
+    u.Nombre, 
+    u.Apellido, 
+    c.CodigoQR, 
+    i.FechaRegistro, 
+    i.Descripcion 
+FROM ingresos i 
+INNER JOIN usuarios u ON i.idUsuario = u.Id 
+INNER JOIN categorias c ON i.IdTipo = c.Id  
+WHERE c.tipo = "ingreso" 
+ORDER BY i.Fecha DESC;';
     $resultado = mysqli_query($conn,$consulta);
 
     if($resultado){
