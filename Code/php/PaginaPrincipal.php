23d704407098bf6c115fe96422b3168c67921df0
AlejandroObando23 <alejoobando21@gmail.com>
2025-03-09 22:29:29 -0500

    auditoriaPagina

--- Code/php/PaginaPrincipal.php
+++ Code/php/PaginaPrincipal.php
@@ -146,7 +146,7 @@ $permisos = isset($_SESSION['permisos']) ? $_SESSION['permisos'] : [];
 
 
                 <li class="nav-item opcion fw-bold" <?php if (!isset($_SESSION['permisos']['PaginaCategorias']) || $_SESSION['permisos']['PaginaCategorias'] != 1) echo 'style="display: none;"'; ?>>
-                    <a id="categorias" class="nav-link" href="#" onclick="cargarPagina('../html/Auditoria/auditoria.html',9)">
+                    <a id="categorias" class="nav-link" href="#" onclick="cargarPagina('Auditoria/Auditoria.php',9)">
                         <i class="bi bi-tags"></i> Auditoria
                     </a>
                 </li>
